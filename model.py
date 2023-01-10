79b6af5eee7cad138ca242a7ddf5f417b529af0e
Stef De Visscher <stef@otiv.ai>
2023-01-10 16:58:13 +0100

    4 layer model

--- model.py
+++ model.py
@@ -29,3 +29,25 @@ class ModelB(nn.Module):
         x = F.relu(x)
         x = self.lin2(x)
         return x
+
+class ModelC(nn.Module):
+    def __init__(self):
+        super(ModelB, self).__init__()
+        self.lin1 = nn.Linear(2, 128)
+        self.lin2 = nn.Linear(128, 128)
+        self.lin3 = nn.Linear(128, 128)
+        self.lin4 = nn.Linear(128, 1)
+
+    def forward(self, x: torch.Tensor):
+        """Forward function for the model.
+        
+        :param x: The input tensor.
+        """
+        x = self.lin1(x)
+        x = F.relu(x)
+        x = self.lin2(x)
+        x = F.relu(x)
+        x = self.lin3(x)
+        x = F.relu(x)
+        x = self.lin4(x)
+        return x
